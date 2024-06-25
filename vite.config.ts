8ab8e328c91845e3a3eb1d9e64972e6468095af1
ArnoSahakyan <sahakyan.arno@gmail.com>
2024-06-25 16:30:26 +0400

    changed backend urls

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080/",
+        target: "https://winkuback.onrender.com/",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
