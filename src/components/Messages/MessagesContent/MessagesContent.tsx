713c3726e62f95f397bdef74a317204662dfd1a8
ArnoSahakyan <sahakyan.arno@gmail.com>
2024-07-02 16:16:38 +0400

    added a check when user doesnt have friends and visits messages page

--- src/components/Messages/MessagesContent/MessagesContent.tsx
+++ src/components/Messages/MessagesContent/MessagesContent.tsx
@@ -1,5 +1,4 @@
 import { useDispatch, useSelector } from 'react-redux'
-import './MessagesContent.scss'
 import { getFriendsBack, getMessagesLoading, receiveMessage } from '../../../store/features/friends/friendsSlice'
 import Friend from '../../FriendsBar/Friend/Friend'
 import { useEffect, useState, useRef } from 'react'
@@ -10,6 +9,7 @@ import { getAccessToken } from '../../../store/features/userInfo/userInfoSlice'
 import io, { Socket } from 'socket.io-client';
 import { AppDispatch } from '../../../store/setup'
 import SecondaryLoader from '../../shared/SecondaryLoader/SecondaryLoader'
+import './MessagesContent.scss'
 const url = import.meta.env.VITE_BACK_BASE_URL;
 
 export default function MessagesContent() {
@@ -72,42 +72,45 @@ export default function MessagesContent() {
   }, [friends, selectedFriendIndex]);
 
   return (
-    <div className='MessagesContent'>
-      <div className="MessagesContent__friends">
-        {
-          friends.map((friend, index) => {
-            return <div key={friend.id} className="friend" onClick={() => selectFriend(index)}>
-              <Friend user={friends[index]} onlyImg={false} />
-            </div>
-          })
-        }
-      </div>
-      <div className="MessagesContent__box">
-        <div className="content">
-          <img src={friends[selectedFriendIndex]?.pfp} alt={friends[selectedFriendIndex]?.fname} />
-          <div className="content-info">
-            <h4>{friends[selectedFriendIndex]?.fname}</h4>
-            <span>{friends[selectedFriendIndex]?.onlineStatus}</span>
+    <div className={`${friends && friends.length === 0 ? "MessagesContent-nomessages" : ''} MessagesContent`}>
+      {
+        friends && friends.length === 0
+          ? <p className='no-users'>add friends and have a chat!</p>
+          : <><div className="MessagesContent__friends">
+            {
+              friends.map((friend, index) => {
+                return <div key={friend.id} className="friend" onClick={() => selectFriend(index)}>
+                  <Friend user={friends[index]} onlyImg={false} />
+                </div>
+              })
+            }
           </div>
-        </div>
+            <div className="MessagesContent__box">
+              <div className="content">
+                <img src={friends[selectedFriendIndex]?.pfp} alt={friends[selectedFriendIndex]?.fname} />
+                <div className="content-info">
+                  <h4>{friends[selectedFriendIndex]?.fname}</h4>
+                  <span>{friends[selectedFriendIndex]?.onlineStatus}</span>
+                </div>
+              </div>
 
-        <div className={`${messageLoading || friends[selectedFriendIndex]?.messages?.length === 0 ? "chat-loading" : ""} chat`} ref={chatRef}>
-          {
-            (messageLoading && <SecondaryLoader />)
-            ||
-            (
-              friends[selectedFriendIndex]?.messages?.length === 0
-                ? <p className='no-users'>there are no messages here yet</p>
-                : friends[selectedFriendIndex]?.messages.map(message => {
-                  return <MessageChat key={message.messageId} content={message} user={friends[selectedFriendIndex]} />
-                })
-            )
-          }
-        </div>
+              <div className={`${messageLoading || friends[selectedFriendIndex]?.messages?.length === 0 ? "chat-loading" : ""} chat`} ref={chatRef}>
+                {
+                  (messageLoading && <SecondaryLoader />)
+                  ||
+                  (
+                    friends[selectedFriendIndex]?.messages?.length === 0
+                      ? <p className='no-users'>there are no messages here yet</p>
+                      : friends[selectedFriendIndex]?.messages.map(message => {
+                        return <MessageChat key={message.messageId} content={message} user={friends[selectedFriendIndex]} />
+                      })
+                  )
+                }
+              </div>
 
-        <MessageInput friend={friends[selectedFriendIndex]} socket={socket} />
-
-      </div>
+              <MessageInput friend={friends[selectedFriendIndex]} socket={socket} />
+            </div></>
+      }
     </div>
   )
 }
